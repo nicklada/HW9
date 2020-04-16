eb3b791ef48d8f2f81d3ce655c358092704e0948
lada <nicklada94@gmail.com>
2020-04-16 19:50:30 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,15 @@
+package ru.netology.domain;
+
+public class Post {
+    private String id;
+    private String nameGroup;
+    private String date;
+    private int time;
+    private String content;
+    private String imageUrl;
+    private int likesInfo;
+    private int repostsInfo;
+    private int viewsInfo;
+    private int commentsInfo;
+    
+}
