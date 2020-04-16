9f68379b72a9918adcbb7fe996c09ad2380b7eb2
lada <nicklada94@gmail.com>
2020-04-16 19:51:39 +0300

    Getters and Setters added

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -11,5 +11,84 @@ public class Post {
     private int repostsInfo;
     private int viewsInfo;
     private int commentsInfo;
-    
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNameGroup() {
+        return nameGroup;
+    }
+
+    public void setNameGroup(String nameGroup) {
+        this.nameGroup = nameGroup;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(int likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public int getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(int repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public int getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(int viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public int getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(int commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
 }
